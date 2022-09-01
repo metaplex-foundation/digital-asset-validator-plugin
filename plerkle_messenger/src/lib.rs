f3497eedf270e3b6438cc083ba58f4d740e405ec
Austin Adams <austbot@users.noreply.github.com>
2022-09-01 16:16:49 -0500

    make errors public and change solana deps (#18)

--- plerkle_messenger/src/lib.rs
+++ plerkle_messenger/src/lib.rs
@@ -1,14 +1,15 @@
 extern crate core;
+mod redis_messenger;
+mod pulsar_messenger;
 
 #[cfg(feature = "redis")]
 pub use crate::redis_messenger::*;
 
 #[cfg(feature = "pulsar")]
-
 pub use crate::pulsar_messenger::*;
 
 mod error;
 mod plerkle_messenger;
-mod redis_messenger;
-mod pulsar_messenger;
+
 pub use crate::plerkle_messenger::*;
+pub use crate::error::*;
