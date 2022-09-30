902de15cf5a8459e0d7a54c73f0a147ba32badeb
austbot <me@austbot.com>
2022-09-30 02:58:38 -0500

    Allo custom geyser config file

--- docker/runs.sh
+++ docker/runs.sh
@@ -14,34 +14,43 @@ websocket_url: ws://localhost:8899
 commitment: finalized
 EOL
 
-cat << EOL > accountsdb-plugin-config.json
-{
-    "libpath": "/plugin/plugin.so",
-    "accounts_selector" : {
-        "accounts" : [
-            "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-            "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
-            "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
-            "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
-            "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
-        ]
-    },
-    "transaction_selector" : {
-        "mentions" : [
-            "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-            "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
-            "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
-            "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
-            "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
-        ]
+
+if [[ ! -f /plugin-config/accountsdb-plugin-config.json ]]
+then
+cat << EOL > /plugin-config/accountsdb-plugin-config.json
+    {
+        "libpath": "/plugin/plugin.so",
+        "accounts_selector" : {
+            "accounts" : [
+                "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+                "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
+                "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+                "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
+                "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
+            ]
+        },
+        "transaction_selector" : {
+            "mentions" : [
+                "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+                "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
+                "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+                "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
+                "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
+            ]
+        }
     }
-}
 EOL
+fi
+
 
 programs=()
-for prog in /so/*; do
-    programs+=("--bpf-program" "$(basename $prog .so)" "$prog")
-done
+if [ "$(ls -A /so)" ]; then
+  for prog in /so/*; do
+      programs+=("--bpf-program" "$(basename $prog .so)" "$prog")
+  done
+fi
 
 export RUST_BACKTRACE=1
 dataDir=$PWD/config/"$(basename "$0" .sh)"
@@ -55,9 +64,9 @@ args=(
   --log
   --reset
   --rpc-port 8899
-  --geyser-plugin-config accountsdb-plugin-config.json
+  --geyser-plugin-config /plugin-config/accountsdb-plugin-config.json
 )
 # shellcheck disable=SC2086
-cat accountsdb-plugin-config.json
+cat /plugin-config/accountsdb-plugin-config.json
 ls -la /so/
 solana-test-validator  "${programs[@]}" "${args[@]}" $SOLANA_RUN_SH_VALIDATOR_ARGS
