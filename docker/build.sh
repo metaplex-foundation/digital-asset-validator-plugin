74f3e0346021f5620093d8673447e1edae45fcbc
vovkman <liam.vovk@gmail.com>
2023-01-25 20:20:10 -0400

    Add script to output solana and geyser artifacts

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# builds the Solana.Dockerfile image and outputs build artefacts to docker-output directory, you can use this
+# to get the geyser plugin build and solana validator build to run on a node
+set -eux
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+docker-compose build
+docker create --name temp digital-asset-validator-plugin_solana:latest
+mkdir -p $SCRIPT_DIR/geyser-outputs
+mkdir -p $SCRIPT_DIR/solana-outputs
+# copy plugin .so file
+docker container cp temp:/plugin $SCRIPT_DIR/geyser-outputs
+# copy solana executables
+docker container cp temp:/usr/bin $SCRIPT_DIR/solana-outputs
+docker rm temp
