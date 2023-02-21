654001add2c2106e6dc9ae2541cc7c347424fa26
austbot <me@austbot.com>
2023-02-21 12:33:08 -0600

    add missing files

---
+++ plerkle_serialization/src/error.rs
@@ -0,0 +1,6 @@
+use thiserror::Error;
+#[derive(Debug, Clone, PartialEq, Eq,Error)]
+pub enum PlerkleSerializationError {
+    #[error("Serialization error: {0}")]
+    SerializationError(String)
+}
